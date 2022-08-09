6349e992bbace6f2832d7bc37dd9347641bb6ea8
Aimen mesfin <91012209+iimenmesfin@users.noreply.github.com>
2022-08-09 22:14:36 +0300

    login form added

---
+++ assignment3/assignment3/assignment3/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dt_registered_date = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.txtnumber = new System.Windows.Forms.TextBox();
+            this.sku = new System.Windows.Forms.TextBox();
+            this.txtobjname = new System.Windows.Forms.TextBox();
+            this.txtcount = new System.Windows.Forms.TextBox();
+            this.txtprice = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(310, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "date";
+            // 
+            // dt_registered_date
+            // 
+            this.dt_registered_date.Location = new System.Drawing.Point(263, 37);
+            this.dt_registered_date.Name = "dt_registered_date";
+            this.dt_registered_date.Size = new System.Drawing.Size(200, 22);
+            this.dt_registered_date.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(582, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "inventory name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "object name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "count";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(335, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "price";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(21, 306);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(78, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(388, 280);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 14;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // txtnumber
+            // 
+            this.txtnumber.Location = new System.Drawing.Point(28, 36);
+            this.txtnumber.Name = "txtnumber";
+            this.txtnumber.Size = new System.Drawing.Size(100, 22);
+            this.txtnumber.TabIndex = 16;
+            // 
+            // sku
+            // 
+            this.sku.Location = new System.Drawing.Point(580, 39);
+            this.sku.Name = "sku";
+            this.sku.Size = new System.Drawing.Size(100, 22);
+            this.sku.TabIndex = 17;
+            // 
+            // txtobjname
+            // 
+            this.txtobjname.Location = new System.Drawing.Point(12, 118);
+            this.txtobjname.Name = "txtobjname";
+            this.txtobjname.Size = new System.Drawing.Size(426, 22);
+            this.txtobjname.TabIndex = 18;
+            // 
+            // txtcount
+            // 
+            this.txtcount.Location = new System.Drawing.Point(12, 249);
+            this.txtcount.Name = "txtcount";
+            this.txtcount.Size = new System.Drawing.Size(100, 22);
+            this.txtcount.TabIndex = 19;
+            // 
+            // txtprice
+            // 
+            this.txtprice.Location = new System.Drawing.Point(286, 200);
+            this.txtprice.Name = "txtprice";
+            this.txtprice.Size = new System.Drawing.Size(125, 22);
+            this.txtprice.TabIndex = 20;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(496, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(571, 362);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "is available ",
+            "not available"});
+            this.checkedListBox1.Location = new System.Drawing.Point(146, 172);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(134, 89);
+            this.checkedListBox1.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(888, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(230, 283);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 20);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.Text = "is available";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 535);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtprice);
+            this.Controls.Add(this.txtcount);
+            this.Controls.Add(this.txtobjname);
+            this.Controls.Add(this.sku);
+            this.Controls.Add(this.txtnumber);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dt_registered_date);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dt_registered_date;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.TextBox txtnumber;
+        private System.Windows.Forms.TextBox sku;
+        private System.Windows.Forms.TextBox txtobjname;
+        private System.Windows.Forms.TextBox txtcount;
+        private System.Windows.Forms.TextBox txtprice;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ assignment3/assignment3/assignment3/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using assignment3.model;
+
+
+namespace assignment3
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string UserName)
+        {
+            InitializeComponent();
+            label7.Text = UserName;
+        }
+
+        product p = new product();
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+
+            Regex reg = new Regex(@"[a-z]{3}$");
+            if (String.IsNullOrEmpty(txtobjname.Text) || txtobjname.Text.Length < 3)
+            {
+                errorProvider1.SetError(txtobjname, "name is required");
+            }
+            else if (reg.IsMatch(txtobjname.Text))
+            {
+                errorProvider1.Clear();
+                product p1 = new product();
+                try
+                {
+                    p1.count = Convert.ToInt32(txtcount.Text);
+                    p1.price = Convert.ToDouble(txtprice.Text);
+                    p1.number = Convert.ToInt32(txtnumber.Text);
+                    p1.inventorynumber = Convert.ToInt32(sku.Text);
+
+                }
+                catch
+                {
+
+                }
+
+                p1.date = dt_registered_date.Text;
+                p1.objname = txtobjname.Text;
+                p1.isavailable = checkBox1.Checked;
+                string msg = " ";
+                foreach (var item in checkedListBox1.CheckedItems)
+                {
+                    msg += item.ToString();
+                }
+                MessageBox.Show(msg + " ");
+        
+
+                
+                
+                        
+                p1.Save();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = product.getallproducts();
+
+            }
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+       
